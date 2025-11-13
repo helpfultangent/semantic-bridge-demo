572831be8682ea3a473a79a6013c054dadc677b9
helpfultangent <helpfultangent@users.noreply.github.com>
2025-11-12 23:22:13 -0600

    Change conda environment name to 'semantic-SVO'

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ set -xe
 # IS_GPU_JOB: Boolean value to indicate if the job is a GPU job. If true, it will load the CUDA module
 export GIT_REPO_URL="https://github.com/helpfultangent/semantic-bridge-demo.git"
 export COOKBOOK_NAME="semantic-bridge-demo"
-export COOKBOOK_CONDA_ENV="example"
+export COOKBOOK_CONDA_ENV="semantic-SVO"
 IS_GPU_JOB=false
 
 
